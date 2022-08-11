1a28af49056682bd1e50d2934667b82f04dd19e2
artik nurochman <artiknurochman@gmail.com>
2022-08-11 22:50:23 +0700

    update teros

--- client/pages/produk/[produkId].js
+++ client/pages/produk/[produkId].js
@@ -1,12 +1,12 @@
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
-const getProduk = ({ produk }) => {
+const getProduk = ({ produkToko }) => {
   const { doRequest, errors } = useRequest({
     url: '/api/orders',
     method: 'post',
     body: {
-      produkId: produk.id,
+      produkId: produkToko.id,
     },
     onSuccess: (order) =>
       Router.push('/orders/[orderId]', `/orders/${order.id}`),
